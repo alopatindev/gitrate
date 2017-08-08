76010f03fe329da0b8624c961e5e49613fcc7f5a
Alexander Lopatin <alopatindev@gmail.com>
2017-08-08 03:10:53 +0300

    refactoring

--- scrapers/find-github-users.py
+++ scrapers/find-github-users.py
@@ -1,119 +1,16 @@
 #!/usr/bin/env python3
 
-QUERY = '''
-query {
-  search(
-    first:3,
-    type:REPOSITORY,
-    query:"in:README.md sort:updated mirror:false fork:false '](https://travis-ci.org/'"
-  ) {
-    nodes {
-      ... on Repository {
-        id
-        name
-        createdAt
-        pushedAt
-        defaultBranchRef {
-          target {
-            ... on Commit {
-              id
-              history(first:3) {
-                nodes {
-                  author {
-                    user {
-                      id
-                      #login
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-        owner {
-          id
-          login
-          pinnedRepositories(first:6, orderBy: { field:UPDATED_AT, direction:DESC }) {
-            nodes {
-              id
-              nameWithOwner
-              isFork
-              isMirror
-              createdAt
-              pushedAt
-              defaultBranchRef {
-                target {
-                  ... on Commit {
-                    history(first:3) {
-                      nodes {
-                        author {
-                          user {
-                            id
-                            #login
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-              repositoryTopics(first:20) {
-                nodes {
-                  topic {
-                    #id
-                    name
-                  }
-                }
-              }
-            }
-          }
-          repositories(first:10, orderBy: { field:UPDATED_AT, direction:DESC }) {
-            nodes {
-              id
-              nameWithOwner
-              isFork
-              isMirror
-              createdAt
-              pushedAt
-              defaultBranchRef {
-                target {
-                  ... on Commit {
-                    history(first:3) {
-                      nodes {
-                        author {
-                          user {
-                            id
-                            #login
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-              repositoryTopics(first:20) {
-                nodes {
-                  topic {
-                    #id
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
-        repositoryTopics(first:20) {
-          nodes {
-            topic {
-              #id
-              name
-            }
-          }
-        }
-      }
-    }
-  }
-}
-'''
+query = "in:README.md sort:updated mirror:false fork:false '](https://travis-ci.org/'"
 
-print(QUERY)
+args = {
+    'query': query,
+    'results_limit': 20,
+    'commits_limit': 2,
+    'topics_limit': 20,
+    'repositories_limit': 20,
+    'pinned_repositories_limit': 6,
+}
+
+graphql_query = open('find-github-users.graphql').read() % args
+
+print(graphql_query)
