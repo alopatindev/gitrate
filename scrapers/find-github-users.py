c88a3e721fab355f3d1868c388e3a5a1fb6f7c2a
Alexander Lopatin <alopatindev@gmail.com>
2017-08-08 07:50:28 +0300

    update

--- scrapers/find-github-users.py
+++ scrapers/find-github-users.py
@@ -4,9 +4,11 @@
 import json
 import requests
 import sys
+from dateutil.parser import parse as isodate
 
 
 GITHUB_TOKEN = sys.argv[1]
+MIN_REPO_AGE_DAYS = 60
 
 
 def gql_execute(query):
@@ -21,16 +23,17 @@ def gql_execute(query):
     return json.loads(response.text)
 
 
-def search_users():
+def search_repositories_and_users():
     pattern = '](https://travis-ci.org/'
     query = "in:README.md sort:updated mirror:false fork:false '%s'" % pattern
     query_args = {
         'search_query': query,
         'results_limit': 20,
         'commits_limit': 2,
-        'topics_limit': 20,
         'repositories_limit': 20,
         'pinned_repositories_limit': 6,
+        'topics_limit': 20,
+        'languages_limit': 20,
         'results_offset': '',
     }
 
@@ -48,9 +51,19 @@ def search_users():
         else:
             break
 
-        yield result
+        yield result['nodes']
 
 
-for result in search_users():
+def extract_users(repositories_and_users):
+    for nodes in repositories_and_users:
+        for node in nodes:
+            pushed_at = isodate(node['pushedAt'])
+            created_at = isodate(node['createdAt'])
+            age = (pushed_at - created_at).days
+            if age >= MIN_REPO_AGE_DAYS:
+                yield node
+
+
+for result in extract_users(search_repositories_and_users()):
     print(result)
     break
