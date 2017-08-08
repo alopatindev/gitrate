ecbbfbd19f86d5fff85b70048303d3f6708b1824
Alexander Lopatin <alopatindev@gmail.com>
2017-08-08 05:30:15 +0300

    make gql request

--- scrapers/find-github-users.py
+++ scrapers/find-github-users.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
+# coding=utf8
 
-query = "in:README.md sort:updated mirror:false fork:false '](https://travis-ci.org/'"
+import json
+import requests
+import sys
 
-args = {
-    'query': query,
-    'results_limit': 20,
-    'commits_limit': 2,
-    'topics_limit': 20,
-    'repositories_limit': 20,
-    'pinned_repositories_limit': 6,
-}
 
-graphql_query = open('find-github-users.graphql').read() % args
+GITHUB_TOKEN = sys.argv[1]
 
-print(graphql_query)
+
+def gql_execute(query):
+    headers = {'Authorization': 'bearer %s' % GITHUB_TOKEN}
+    data = {'query': query}
+    json_data = json.dumps(data)
+    response = requests.post(
+        url='https://api.github.com/graphql',
+        headers=headers,
+        data=json_data
+    )
+    return json.loads(response.text)
+
+
+def search_users():
+    pattern = '](https://travis-ci.org/'
+    query = "in:README.md sort:updated mirror:false fork:false '%s'" % pattern
+    query_args = {
+        'search_query': query,
+        'results_limit': 20,
+        'commits_limit': 2,
+        'topics_limit': 20,
+        'repositories_limit': 20,
+        'pinned_repositories_limit': 6,
+    }
+    graphql_query = open('find-github-users.graphql').read() % query_args
+    return gql_execute(graphql_query)
+
+
+print(search_users())
