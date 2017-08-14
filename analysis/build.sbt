9ce29dc7ee47a57c19a39234a4e6df6d93627028
Alexander Lopatin <alopatindev@gmail.com>
2017-08-14 15:27:35 +0300

    scalatest: use WordSpec

--- analysis/build.sbt
+++ analysis/build.sbt
@@ -36,7 +36,6 @@ libraryDependencies ++= Seq(
 
   "org.scalatest" %% "scalatest" % "3.0.3" % Test,
   "org.scalacheck" %% "scalacheck" % "1.13.5" % Test,
-  "junit" % "junit" % "4.12" % Test,
 )
 
 dependencyOverrides ++= Seq(
--- analysis/src/test/scala/utils/StringUtilsSuite.scala
+++ analysis/src/test/scala/utils/StringUtilsSuite.scala
@@ -1,22 +1,33 @@
 package hiregooddevs.utils
 
-import org.junit.runner.RunWith
-import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
 import org.scalatest.Matchers._
-import org.scalatest.prop.Checkers
+import org.scalatest.WordSpec
 
-@RunWith(classOf[JUnitRunner])
-class StringUtilsSuite extends FunSuite with Checkers {
+class StringUtilsSuite extends WordSpec {
 
   import StringUtils._
 
-  test("formatTemplate should replace placeholders with text") {
-    "".formatTemplate(Map()) shouldEqual ""
-    "Hello".formatTemplate(Map()) shouldEqual "Hello"
-    "Hello".formatTemplate(Map("unused" -> "value")) shouldEqual "Hello"
-    "Hello ${key}!".formatTemplate(Map("key" -> "World")) shouldEqual "Hello World!"
-    "Hello ${key}!".formatTemplate(Map("key" -> "World", "unused" -> "value")) shouldEqual "Hello World!"
+  "StringUtils" can {
+
+    "formatTemplate" should {
+      "left string unchanged when no placeholders" in {
+        "".formatTemplate(Map()) shouldEqual ""
+        "Hello".formatTemplate(Map()) shouldEqual "Hello"
+        "Hello".formatTemplate(Map("unused" -> "value")) shouldEqual "Hello"
+        "Hello ${key}!".formatTemplate(Map()) shouldEqual "Hello ${key}!"
+      }
+
+      "replace placeholders with text" in {
+        "Hello ${key}!".formatTemplate(Map("key" -> "World")) shouldEqual "Hello World!"
+        "Hello ${key}!".formatTemplate(Map(
+          "key" -> "World",
+          "unused" -> "value")) shouldEqual "Hello World!"
+        "Hello ${key}${anotherKey}".formatTemplate(Map(
+          "key" -> "World",
+          "anotherKey" -> "!")) shouldEqual "Hello World!"
+      }
+    }
+
   }
 
 }
