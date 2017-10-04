844fb9e3063fafbe544861c31df1bd3eef4ea241
Alexander Lopatin <alopatindev@gmail.com>
2017-10-05 02:16:44 +0300

    refactoring

--- analysis/scripts/downloadAndAnalyzeCode.sh
+++ analysis/scripts/downloadAndAnalyzeCode.sh
@@ -3,101 +3,29 @@
 # (stdin) input format: "repository_id;repository_name;login;archive_uri;language1,language2,..."
 # (stdout) output format: "repository_id;language;message_type;message"
 
-function analyze_javascript () {
-    repository_id="$1"
-    repository_name="$2"
-    login="$3"
-    archive_output_dir="$4"
-
-    function output () {
-        language="JavaScript"
-        message_type="$1"
-        message="$2"
-        echo "${repository_id};${language};${message_type};${message}"
-    }
-
-    valid_git_url=true
-    for packagejson in $(find "${archive_output_dir}" -type f -regextype posix-extended \
-        -regex '.*/(package|bower)\.json$'); do
-        git_url=$(jq --monochrome-output --raw-output ".repository.url" "${packagejson}")
-        if [ "${git_url}" = null ]; then
-            continue
-        fi
-        git_url_match=$(echo "${git_url}" | grep -E "[:/]${login}/${repository_name}")
-        if [ "${git_url_match}" != "${git_url}" ]; then
-            valid_git_url=false
-        fi
-    done
-
-    if [ "${valid_git_url}" = true ] ; then
-        packagejson_dir=
-        for packagejson in $(find "${archive_output_dir}" -type f -regextype posix-extended \
-            -regex '.*/(package|bower)\.json$'); do
-            dir=$(dirname "${packagejson}")
-            if [ "${packagejson_dir}" = "" ] || [ ${packagejson_dir} = "${dir}" ]; then
-                packagejson_dir="${dir}"
-                for key in "dependencies" "devDependencies" ; do
-                    for dep in $(jq --monochrome-output --raw-output ".${key} | keys[]" "${packagejson}"); do
-                        output dependence "${dep}"
-                    done
-                done
-
-                if (jq --monochrome-output --raw-output ".scripts | values[]" "${packagejson}" | grep -E "^node " || \
-                    grep -RE "^#!/usr/bin/(env |)node" "${archive_output_dir}") >> /dev/null; then
-                    output dependence "Node.js"
-                fi
-            else
-                # that's probably a third-party library that was copy-pasted to the repository
-                rm -r "${dir}"
-            fi
-            rm "${packagejson}"
-        done
-
-        if [ "${packagejson_dir}" != "" ]; then
-            find "${archive_output_dir}" \
-                -type f \
-                -regextype posix-extended \
-                -regex '.*/(\.eslint.*|yarn\.lock|.*\.min\.js|package-lock\.json|\.gitignore)$' \
-                -delete
-
-            find "${archive_output_dir}" -type f -name "*.js" -exec node "stripComments.js" "{}" ";"
-
-            for message in $(node_modules/eslint/bin/eslint.js --format json --no-color "${archive_output_dir}" | \
-                grep --extended-regexp '^\[' | \
-                jq --monochrome-output --raw-output '.[].messages[] | "\(.ruleId)"'); do
-                output warning "${message}"
-            done
-
-            lines_of_code=$(find "${archive_output_dir}" -type f -name "*.js" -print0 | \
-                xargs -0 grep --invert-match --regexp='^\s*$' | wc -l)
-            output lines_of_code "${lines_of_code}"
-        fi
-    fi
-}
-
 function analyze () {
-    repository_id=$(echo "$1" | cut -d ";" -f1)
-    repository_name=$(echo "$1" | cut -d ";" -f2)
-    login=$(echo "$1" | cut -d ";" -f3)
-    archive_url=$(echo "$1" | cut -d ";" -f4)
-    languages=$(echo "$1" | cut -d ";" -f5)
+    local repository_id=$(echo "$1" | cut -d ";" -f1)
+    local repository_name=$(echo "$1" | cut -d ";" -f2)
+    local login=$(echo "$1" | cut -d ";" -f3)
+    local archive_url=$(echo "$1" | cut -d ";" -f4)
+    local languages=$(echo "$1" | cut -d ";" -f5)
 
-    archive_output_dir="${current_dir}/data/${repository_id}"
-    archive_path="${archive_output_dir}.tar.gz"
+    local archive_output_dir="${current_dir}/data/${repository_id}"
+    local archive_path="${archive_output_dir}.tar.gz"
     wget --quiet "${archive_url}" -O "${archive_path}"
     rm -rf "${archive_output_dir}"
     mkdir -p "${archive_output_dir}"
     tar -xzf "${archive_path}" -C "${archive_output_dir}"
     rm "${archive_path}"
 
-    good_filename_pattern="^[a-zA-Z0-9/._-]*$"
-    bad_filenames=$(find "${archive_output_dir}" -type f | \
+    local valid_filename_pattern="^[a-zA-Z0-9/._-]*$"
+    local invalid_filenames=$(find "${archive_output_dir}" -type f | \
         sed "s!.*${repository_id}!!" | \
-        grep --count --invert-match --extended-regexp "${good_filename_pattern}")
+        grep --count --invert-match --extended-regexp "${valid_filename_pattern}")
 
-    if [ "${bad_filenames}" -eq 0 ]; then
+    if [ "${invalid_filenames}" -eq 0 ]; then
         for language in $(echo "${languages}" | tr "," "\\n"); do
-            if [[ "${language}" = JavaScript ]]; then
+            if [[ "${language}" = "JavaScript" ]]; then
                 analyze_javascript "${repository_id}" "${repository_name}" "${login}" "${archive_output_dir}"
             fi
         done
@@ -121,6 +49,8 @@ done
 
 # redirect stderr to null
 exec 2>/dev/null
+
+source languages/*.sh
 
 while read -r line; do
     analyze "${line}"
---
+++ analysis/scripts/languages/analyzeJavaScript.sh
@@ -0,0 +1,122 @@
+language="JavaScript"
+
+function has_valid_git_url () {
+    local -a packagejson_files="$1"
+    local repository_name="$2"
+    local login="$3"
+    local valid_git_url=true
+
+    for packagejson in ${packagejson_files[@]}; do
+        local git_url=$(jq --monochrome-output --raw-output ".repository.url" "${packagejson}")
+        if [ "${git_url}" = "" ] || [ "${git_url}" = null ]; then
+            continue
+        fi
+
+        local git_url_match=$(echo "${git_url}" | grep -E "[:/]${login}/${repository_name}")
+        if [ "${git_url_match}" != "${git_url}" ]; then
+            valid_git_url=false
+        fi
+
+        break
+    done
+
+    echo "${valid_git_url}"
+}
+
+function detect_dependencies () {
+    local packagejson="$1"
+    local archive_output_dir="$2"
+    local -a dependencies=()
+
+    for key in "dependencies" "devDependencies" ; do
+        for dep in $(jq --monochrome-output --raw-output ".${key} | keys[]" "${packagejson}"); do
+            dependencies+=("${dep}")
+        done
+    done
+
+    if (jq --monochrome-output --raw-output ".scripts | values[]" "${packagejson}" | grep -E "^node " || \
+        grep -RE "^#!/usr/bin/(env |)node" "${archive_output_dir}") >> /dev/null; then
+        dependencies+=("Node.js")
+    fi
+
+    echo "${dependencies[@]}"
+}
+
+function prepare_sources () {
+    local archive_output_dir="$1"
+
+    find "${archive_output_dir}" \
+        -type f \
+        -regextype posix-extended \
+        -regex '.*/(\.eslint.*|yarn\.lock|.*\.min\.js|package-lock\.json|\.gitignore)$' \
+        -delete
+
+    find "${archive_output_dir}" -type f -name "*.js" -exec node "stripComments.js" "{}" ";"
+}
+
+function compute_lines_of_code () {
+    local archive_output_dir="$1"
+    find "${archive_output_dir}" -type f -name "*.js" -print0 | \
+        xargs -0 grep --invert-match --regexp='^\s*$' | \
+        wc -l
+}
+
+function sort_by_length () {
+    echo "$1" | \
+        awk '{ print length, $0 }' | \
+        sort --numeric-sort --stable | \
+        cut --delimiter=" " --fields="2-"
+}
+
+function analyze_javascript () {
+    local repository_id="$1"
+    local repository_name="$2"
+    local login="$3"
+    local archive_output_dir="$4"
+
+    function output () {
+        local message_type="$1"
+        local message="$2"
+        echo "${repository_id};${language};${message_type};${message}"
+    }
+
+    local -a unsorted_packagejson_files=$(find "${archive_output_dir}" \
+        -type f \
+        -regextype posix-extended -regex '.*/(package|bower)\.json$')
+
+    local -a packagejson_files=$(sort_by_length ${unsorted_packagejson_files[@]})
+
+    if [ "${packagejson_files[@]}" = "" ] ; then
+        return
+    fi
+
+    if [ $(has_valid_git_url "${packagejson_files[@]}" "${repository_name}" "${login}") = true ] ; then
+        local packagejson_dir=
+        for packagejson in ${packagejson_files[@]}; do
+            local dir=$(dirname "${packagejson}")
+            if [ "${packagejson_dir}" = "" ] || [ ${packagejson_dir} = "${dir}" ]; then
+                packagejson_dir="${dir}"
+                local -a dependencies=$(detect_dependencies "${packagejson}" "${archive_output_dir}")
+                for dep in ${dependencies[@]}; do
+                    output dependence "${dep}"
+                done
+            else
+                # that's probably a third-party library that was copy-pasted to the repository
+                rm -r "${dir}"
+            fi
+            rm "${packagejson}"
+        done
+
+        if [ "${packagejson_dir}" != "" ]; then
+            prepare_sources "${archive_output_dir}"
+
+            for message in $(node_modules/eslint/bin/eslint.js --format json --no-color "${archive_output_dir}" | \
+                grep --extended-regexp '^\[' | \
+                jq --monochrome-output --raw-output '.[].messages[] | "\(.ruleId)"'); do
+                output warning "${message}"
+            done
+
+            output lines_of_code "$(compute_lines_of_code "${archive_output_dir}")"
+        fi
+    fi
+}
