8c094ced423d9d7bb25462036a1863faf9384d71
Alexander Lopatin <alopatindev@gmail.com>
2017-10-27 08:31:09 +0300

    tune deployment

--- analysis/deploy.sh
+++ analysis/deploy.sh
@@ -6,18 +6,26 @@ current_dir=$(dirname "$0")
 cd "${current_dir}" || exit 1
 
 CONFIG_PATH="src/main/resources/application.conf"
-APP_NAME="$(grep 'app\.name' "${CONFIG_PATH}" | cut -d ' ' -f3)"
-SCRIPTS_DIR="$(grep 'app\.scriptsDir' "${CONFIG_PATH}" | cut -d ' ' -f3)"
-MAIN_CLASS="gitrate.analysis.Main"
-OUT_JAR="${SCRIPTS_DIR}/out.jar"
+APP_NAME="$(grep '^app\.name = ' "${CONFIG_PATH}" | cut -d ' ' -f3)"
+SCRIPTS_DIR="/tmp/${APP_NAME}"
+CHECKPOINT_DIR="/var/tmp/${APP_NAME}"
+LOG_FILE="/var/tmp/${APP_NAME}.log"
 
-rm -rvf "${SCRIPTS_DIR}"
+MAIN_CLASS="analysis.Main"
+OUT_JAR="${SCRIPTS_DIR}/out.jar"
+SBT_OPTS="-server -Xms3000M -Xmx3000M -Xss1M -XX:+UseConcMarkSweepGC -XX:NewRatio=8"
+
+sudo rm -rvf "${SCRIPTS_DIR}" "${CHECKPOINT_DIR}" "${LOG_FILE}"
 cp -rv scripts "${SCRIPTS_DIR}"
 mkdir -p "${SCRIPTS_DIR}/data"
 
+LOG4G_PROPS="${SCRIPTS_DIR}/log4j-prod.properties"
+
+sudo chown spark:hadoop "${SCRIPTS_DIR}/data"
+
 sbt assembly
-cp -v ./target/scala-*/*-assembly-*.jar "${OUT_JAR}"
-cp -v ./conf/spark-defaults.conf "${SCRIPTS_DIR}/"
+cp -vf ./target/scala-*/*-assembly-*.jar "${OUT_JAR}"
+cp -vf ./conf/spark-defaults.conf "${SCRIPTS_DIR}/"
 
 cd "${SCRIPTS_DIR}"
 
@@ -29,4 +37,6 @@ spark-submit \
     --class "${MAIN_CLASS}" \
     --name "${APP_NAME}" \
     --properties-file spark-defaults.conf \
+    --driver-java-options "-Dlog4j.configuration=file:${LOG4G_PROPS}" \
+    --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:${LOG4G_PROPS}" \
     "${OUT_JAR}"
