311ac9da38a831924425837d69013f0f70e095fb
Alexander Lopatin <alopatindev@gmail.com>
2017-10-27 15:48:13 +0300

    tune deployment

--- analysis/deploy.sh
+++ analysis/deploy.sh
@@ -9,34 +9,45 @@ CONFIG_PATH="src/main/resources/application.conf"
 APP_NAME="$(grep '^app\.name = ' "${CONFIG_PATH}" | cut -d ' ' -f3)"
 SCRIPTS_DIR="/tmp/${APP_NAME}"
 CHECKPOINT_DIR="/var/tmp/${APP_NAME}"
-LOG_FILE="/var/tmp/${APP_NAME}.log"
+
+LOG_FILE_DRIVER="/var/tmp/${APP_NAME}-driver.log"
+LOG_FILE_EXECUTOR="/var/tmp/${APP_NAME}-executor.log"
+LOG4G_PROPS_DRIVER="${SCRIPTS_DIR}/log4j-prod-driver.properties"
+LOG4G_PROPS_EXECUTOR="${SCRIPTS_DIR}/log4j-prod-executor.properties"
 
 MAIN_CLASS="analysis.Main"
 OUT_JAR="${SCRIPTS_DIR}/out.jar"
 SBT_OPTS="-server -Xms3000M -Xmx3000M -Xss1M -XX:+UseConcMarkSweepGC -XX:NewRatio=8"
 
-sudo rm -rvf "${SCRIPTS_DIR}" "${CHECKPOINT_DIR}" "${LOG_FILE}"
-cp -rv scripts "${SCRIPTS_DIR}"
-mkdir -p "${SCRIPTS_DIR}/data"
+WITH_CLEANUP=true
 
-LOG4G_PROPS="${SCRIPTS_DIR}/log4j-prod.properties"
+reset
 
-sudo chown spark:hadoop "${SCRIPTS_DIR}/data"
+if [ "${WITH_CLEANUP}" = true ] ; then
+    sudo rm -rvf "${SCRIPTS_DIR}" "${CHECKPOINT_DIR}" "${LOG_FILE_DRIVER}" "${LOG_FILE_EXECUTOR}"
+    cp -rv scripts "${SCRIPTS_DIR}"
+    mkdir -p "${SCRIPTS_DIR}/data"
 
-sbt assembly
-cp -vf ./target/scala-*/*-assembly-*.jar "${OUT_JAR}"
-cp -vf ./conf/spark-defaults.conf "${SCRIPTS_DIR}/"
+    sudo chown spark:hadoop "${SCRIPTS_DIR}/data"
 
-cd "${SCRIPTS_DIR}"
+    sbt assembly
+    cp -vf ./target/scala-*/*-assembly-*.jar "${OUT_JAR}"
+    cp -vf ./conf/spark-defaults.conf "${SCRIPTS_DIR}/"
 
-# TODO: versions
-npm install eslint eslint-plugin-better eslint-plugin-mocha eslint-plugin-private-props eslint-plugin-promise decomment
+    cd "${SCRIPTS_DIR}"
+
+    # TODO: versions
+    npm install eslint eslint-plugin-better eslint-plugin-mocha eslint-plugin-private-props eslint-plugin-promise decomment
+else
+    cd "${SCRIPTS_DIR}"
+fi
 
 spark-submit \
     --deploy-mode cluster \
     --class "${MAIN_CLASS}" \
     --name "${APP_NAME}" \
     --properties-file spark-defaults.conf \
-    --driver-java-options "-Dlog4j.configuration=file:${LOG4G_PROPS}" \
-    --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:${LOG4G_PROPS}" \
+    --driver-java-options "-Dlog4j.configuration=file:${LOG4G_PROPS_DRIVER}" \
+    --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:${LOG4G_PROPS_EXECUTOR}" \
+    --supervise \
     "${OUT_JAR}"
